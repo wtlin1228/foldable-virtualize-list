cea7007485496c9168efd25b3298c04ef7529a66
weitang.lin <weitang.lin@mujin.co.jp>
2025-04-28 09:37:45 +0900

    working

--- src/data-control.ts
+++ src/data-control.ts
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 const GROUP_DURATION = 2;
 
@@ -24,83 +24,175 @@ type Row =
       data: Entry;
     };
 
-const createGroupFromEntry = (entry: Entry, firstEntryIndex: number): Group => {
-  return {
-    name: entry.time.toString(),
-    isExpanded: false,
-    firstEntryIndex,
-    entryCount: 1,
-  };
-};
+class SegmentTreeNode {}
 
-class SegmentTree {}
+class SegmentTree {
+  private root: null | SegmentTreeNode;
 
-class EntryGroup {
+  constructor() {
+    this.root = null;
+  }
+
+  add(value: number) {
+    throw new Error("not implemented");
+  }
+
+  set(index: number, value: number) {
+    throw new Error("not implemented");
+  }
+
+  search(value: number): { index: number; offset: number } {
+    throw new Error("not implemented");
+  }
+}
+
+export class EntryGroup {
   private entries: Entry[];
   private groups: Group[];
   private segmentTree: SegmentTree;
   private groupDuration: number;
+  private visibleItemCount: number;
 
   constructor(segmentTree: SegmentTree, groupDuration: number) {
     this.entries = [];
     this.groups = [];
     this.segmentTree = segmentTree;
     this.groupDuration = groupDuration;
+    this.visibleItemCount = 0;
   }
 
-  appendEntries(newEntries: Entry[]): void {
-    let prevEntryTime: number =
-      this.entries.length === 0
-        ? -Infinity
-        : this.entries[this.entries.length - 1].time;
+  private appendNewGroup(entry: Entry, firstEntryIndex: number): void {
+    const newGroup = {
+      name: entry.time.toString(),
+      isExpanded: false,
+      firstEntryIndex,
+      entryCount: 1,
+    };
+    this.groups.push(newGroup);
+    this.segmentTree.add(1);
+  }
 
-    for (let i = 0; i < newEntries.length; i++) {
-      const newEntry = newEntries[i];
-      if (newEntry.time < prevEntryTime + this.groupDuration) {
-        this.groups[this.groups.length - 1].entryCount += 1;
-        this.segmentTree.set(groupIndex, group.entryCount);
-      } else {
-        const newGroup = createGroupFromEntry(
-          newEntry,
-          this.entries.length - 1 + i
-        );
-        this.groups.push(newGroup);
-        this.segmentTree.add(group);
-      }
-      prevEntryTime = newEntry.time;
+  private syncLastGroupIfNeeded(): void {
+    const prevGroup = this.groups[this.groups.length - 1];
+    if (prevGroup.isExpanded) {
+      this.segmentTree.set(this.groups.length - 1, prevGroup.entryCount);
+    }
+  }
+
+  private assertGroupExist(groupIndex: number): void {
+    if (groupIndex > this.groups.length - 1) {
+      throw new Error(
+        `failed to fold group(${groupIndex}) since the maximum group index is ${
+          this.groups.length - 1
+        }`
+      );
+    }
+  }
+
+  private getLastGroup(): Group {
+    this.assertGroupExist(this.groups.length - 1);
+    return this.groups[this.groups.length - 1];
+  }
+
+  private getGroup(groupIndex: number): Group {
+    this.assertGroupExist(groupIndex);
+    return this.groups[groupIndex];
+  }
+
+  private getLastEntry(): Entry {
+    return this.entries[this.entries.length - 1];
+  }
+
+  public getVisibleItemCount(): number {
+    return this.visibleItemCount;
+  }
+
+  public foldGroup(groupIndex: number): void {
+    const group = this.getGroup(groupIndex);
+    if (!group.isExpanded) {
+      return;
+    }
+    group.isExpanded = false;
+    this.visibleItemCount -= group.entryCount;
+    this.segmentTree.set(groupIndex, 1);
+  }
+
+  public expandGroup(groupIndex: number): void {
+    const group = this.getGroup(groupIndex);
+    if (group.isExpanded) {
+      return;
+    }
+    group.isExpanded = false;
+    this.visibleItemCount += group.entryCount;
+    this.segmentTree.set(groupIndex, 1 + group.entryCount);
+  }
+
+  public appendEntries(newEntries: Entry[]): void {
+    let i = 0;
+    const nextEntryIndex = this.entries.length;
+
+    if (nextEntryIndex === 0) {
+      this.appendNewGroup(newEntries[0], 0);
+      this.entries.push(newEntries[0]);
+      i += 1;
     }
 
-    this.entries.push(...newEntries);
+    while (i < newEntries.length) {
+      const newEntry = newEntries[i];
+
+      const canMergeIntoLastGroup =
+        newEntry.time < this.getLastEntry().time + this.groupDuration;
+
+      if (canMergeIntoLastGroup) {
+        const lastGroup = this.getLastGroup();
+        lastGroup.entryCount += 1;
+        if (lastGroup.isExpanded) {
+          this.visibleItemCount += 1;
+        }
+      } else {
+        this.syncLastGroupIfNeeded();
+        this.appendNewGroup(newEntry, nextEntryIndex + i);
+        this.visibleItemCount += 1;
+      }
+
+      this.entries.push(newEntry);
+      i += 1;
+    }
+
+    this.syncLastGroupIfNeeded();
   }
 }
 
 export const useDataControl = () => {
-  const [count, setCount] = useState(0);
+  // to force rerender after the internal data updated
+  const [, dummy] = useState({});
+  const forceRerender = useCallback(() => dummy({}), []);
 
-  // use ref to avoid copying array
-  const entriesRef = useRef<Entry[]>([]);
-  const groupsRef = useRef<Group[]>([]);
+  const entryGroup = useMemo(() => {
+    return new EntryGroup(new SegmentTree(), 2);
+  }, []);
 
-  return {
-    append: (more: Entry[]) => {
-      const currentEntriesCount = entriesRef.current.length;
-      let prevEntryTime: number =
-        currentEntriesCount === 0
-          ? -Infinity
-          : entriesRef.current[currentEntriesCount - 1].time;
-
-      for (const entry of more) {
-        if (entry.time < prevEntryTime + GROUP_DURATION) {
-        }
-        prevEntryTime = entry.time;
-      }
-
-      // keep those entries in memory
-      entriesRef.current.push(...more);
-    },
-    getDataCount: () => count,
-    getRowByIndex: (idx: number): Row => {
-      return { type: "entry", data: { content: "fake content", time: 1 } };
-    },
-  };
+  return useMemo(
+    () => ({
+      fold: (groupIndex: number) => {
+        entryGroup.foldGroup(groupIndex);
+        forceRerender();
+      },
+      expand: (groupIndex: number) => {
+        entryGroup.expandGroup(groupIndex);
+        forceRerender();
+      },
+      append: (more: Entry[]) => {
+        entryGroup.appendEntries(more);
+        forceRerender();
+      },
+      getDataCount: () => {
+        return entryGroup.getVisibleItemCount();
+      },
+      getRowByIndex: (idx: number): Row => {
+        return { type: "entry", data: { content: "fake content", time: 1 } };
+      },
+    }),
+    [entryGroup]
+  );
 };
